79da3f4f93458361f9e0e91ff501c1d828227cf3
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2020-09-21 16:27:48 -0300

    User Model Done

--- src/config/config.php
+++ src/config/config.php
@@ -5,3 +5,5 @@ setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'portuguese');
 
 // Pastas
 define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models'));
+
+require_once(realpath(dirname(__FILE__) . '/database.php'));
