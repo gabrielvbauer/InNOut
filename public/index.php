580ba42a3498026e5e311c271ae88fb7d8e3e549
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2020-09-23 19:14:09 -0300

    I cant take it anymore help

--- public/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-require_once(dirname(__FILE__, 2) . '/src/models/User.php');
-
-// $user = new User(['name' => 'Lucas', 'email' => 'lucas@coder.com.br']);
-// echo $user->getSelect(['name' => 'Chaves'], 'name, email');
