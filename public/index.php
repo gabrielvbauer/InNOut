f446de67eafbc39e3515efc64d9c47e3bbd533c9
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2020-09-25 11:19:57 -0300

    Login screen & Select from database

--- public/index.php
+++ public/index.php
@@ -1,11 +1,4 @@
 <?php
 
-require_once(dirname(__FILE__, 2) . '/src/config/database.php');
-
-$sql = 'select * from users';
-$result = Database::getResultFromQuery($sql);
-
-while($row = $result->fetch_assoc()){
-  print_r($row);
-  echo '<br>';
-}
+require_once(dirname(__FILE__, 2) . '/src/config/config.php');
+require_once(dirname(__FILE__, 2) . '/src/views/login.php');
